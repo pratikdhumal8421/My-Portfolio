fe60c5131c4afc6efab97d4c229b7f9bc8da2765
Pratik Dhumal <76606988+pratikdhumal8421@users.noreply.github.com>
2022-05-16 09:08:40 +0530

    Update Images.js

--- Imgaes/Images.js
+++ Imgaes/Images.js
@@ -1,1 +1,8 @@
 You can upload your images
+
+and Sets the path 
+Procedure
+1) Crate Assets folder-> To upload images 
+
+My Assets sections folder images
+https://drive.google.com/drive/folders/1QbnoqFpbMsbcuMY_6fYJDw3Rz6KtR9qv?usp=sharing
